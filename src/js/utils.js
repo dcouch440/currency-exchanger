c2b2862385fd7cfdd62b2c84687cec45f6ba57bf
dcouch440 <dcouch440@gmail.com>
2021-02-12 10:10:20 -0800

    make function for valid keys

---
+++ src/js/utils.js
@@ -0,0 +1,11 @@
+export const currencyConvert = (amount, rate) => amount * rate;
+
+export const isValid = (input, object) => {
+  let isKey = false;
+  for (let k of Object.keys(object)){
+    if (k == input) {
+      isKey = true;
+    }
+  }
+  return isKey;
+};
