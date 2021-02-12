e44e6f0cef0c2ffa6b52b5dc520888738b64550c
dcouch440 <dcouch440@gmail.com>
2021-02-12 09:42:31 -0800

    add destructured values

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,10 @@
 import {CurrencyService} from './services/currency-service.js';
 import '../css/styles.css';
-
-CurrencyService;
-
+/* eslint-disable */
+const myFunction = async () => {
+  const data =  await CurrencyService.request();
+  console.log(data);
+  const {base_code, time_last_update_utc, time_next_update_utc, conversion_rates} = data;
+  console.log(conversion_rates);
+};
+myFunction()
